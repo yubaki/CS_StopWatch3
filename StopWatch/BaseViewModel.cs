c09e6ab03e047fd7d8e892d6dff2c188f4d5fd19
yubaki <yellowtarget12@gmail.com>
2017-01-19 23:58:38 +0900

    stopwatch commit

---
+++ StopWatch/BaseViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StopWatch
+{
+     public class BaseViewModel : INotifyPropertyChanged, IDataErrorInfo
+    {
+        // INotifyPropertyChangedインタフェースを実装する
+        // event構文を用いることで、Add,Deleteを省略できる。
+        // PropertyのDelegate版
+
+        /// <summary>
+        /// プロパティ値が変更されたことをクライアントに通知します。
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // 例１ 
+
+        /// <summary>
+        /// <para>PropertyChanged イベント を発生させます。</para>
+        /// <para>CallerMemberNameからプロパティ名を取得。</para>
+        /// </summary>
+        /// <param name="propertyName">変更されたプロパティの名前</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// PropertyChanged イベント を発生させます。
+        /// </summary>
+        /// <param name="propertyName">変更されたプロパティの名前</param>
+        //protected void OnPropertyChanged(string propertyName)
+        //{
+        //    if (this.PropertyChanged != null)
+        //    {
+        //        this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
+
+        // 例２
+        
+        /// <summary>
+        /// <para>PropertyChanged イベント を発生させます。</para>
+        /// <para>Expression<Func<T>>からプロパティを取得し、プロパティ名を取得</para>
+        /// </summary>
+        /// <param name="propertyName">変更されたプロパティ</param>
+        protected void OnPropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyExpression.Name));
+            }
+        }
+
+        // IDataErrorInfoインタフェースを実装する
+        //0902
+
+        //IDataErrorInfo用のエラーメッセージを保持する辞書
+        private Dictionary<string, string> _ErrorMessages = new Dictionary<string, string>();
+
+        //IDataErrorInfo.Errorの実装
+        string IDataErrorInfo.Error
+        {
+            get { return (_ErrorMessages.Count > 0) ? "Has Error" : null; }
+        }
+
+        //IDataErrorInfo.Itemの実装    実際にエラーメッセージを取り出す際に必要な記述
+        string IDataErrorInfo.this[string columnName]
+        {
+            get
+            {
+                if (_ErrorMessages.ContainsKey(columnName))
+                    return _ErrorMessages[columnName];
+                else
+                    return null;
+            }
+        }
+
+        //エラーメッセージのセット
+        protected void SetError(string propertyName, string errorMessage)
+        {
+            _ErrorMessages[propertyName] = errorMessage;
+        }
+
+        //エラーメッセージのクリア
+        protected void ClearError(string propertyName)
+        {
+            if (_ErrorMessages.ContainsKey(propertyName))
+                _ErrorMessages.Remove(propertyName);
+        }
+
+        //エラーメッセージが入っているかどうか
+        /// <summary>
+        /// エラーメッセージが入っているかどうか
+        /// </summary>
+        /// <param name="propertyName">プロパティ名</param>
+        /// <returns>登録ずみならtrue</returns>
+        protected bool CheckError(string propertyName)
+        {
+            bool ret = _ErrorMessages.ContainsKey(propertyName);
+            return ret;
+        }
+    }
+}
---
+++ StopWatch/StopWatchViewModel.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace StopWatch
+{
+    class StopWatchViewModel : BaseViewModel
+    {
+        DispatcherTimer dispatcherTimer;
+        DateTime StartTime;
+        TimeSpan starttime;
+
+        /// <summary>
+        /// 経過時間表示
+        /// </summary>
+        private string dispTime;
+
+        /// <summary>
+        /// 経過時間表示
+        /// </summary>
+        public string DispTime
+        {
+            set
+            {
+                dispTime = value;
+                OnPropertyChanged();
+            }
+            get
+            {
+                return dispTime;
+            }
+        }
+
+        private bool stopFlag;
+
+        /// 起動コマンド
+        /// </summary>
+        private ICommand start;
+
+        /// <summary>
+        /// 起動コマンド
+        /// </summary>
+        public ICommand Start
+        {
+            get
+            {
+                if (null == start)
+                {
+                    start = new DelegateCommand(StartWatch);
+
+                }
+                return start;
+            }
+        }
+
+        /// 停止コマンド
+        /// </summary>
+        private ICommand stop;
+
+        /// <summary>
+        /// 停止コマンド
+        /// </summary>
+        public ICommand Stop
+        {
+            get
+            {
+                if (null == stop)
+                {
+                    stop = new DelegateCommand(StopWatch);
+
+                }
+                return stop;
+            }
+        }
+
+        /// リセットコマンド
+        /// </summary>
+        private ICommand reset;
+
+        /// <summary>
+        /// リセットコマンド
+        /// </summary>
+        public ICommand Reset
+        {
+            get
+            {
+                if (null == reset)
+                {
+                    reset = new DelegateCommand(ResetWatch);
+
+                }
+                return reset;
+            }
+        }
+
+        /// <summary>
+        /// タイマー起動
+        /// </summary>
+        private void StartWatch()
+        {
+            if (stopFlag == true)
+            {
+                StartTime = DateTime.Now;
+                stopFlag = false;
+            }
+            dispatcherTimer.Start();
+        }
+
+        /// <summary>
+        /// タイマー停止
+        /// </summary>
+        private void StopWatch()
+        {
+            if (stopFlag == false)
+            {
+                dispatcherTimer.Stop();
+                stopFlag = true;
+                //StartTime = DateTime.MinValue;
+            }
+        }
+
+        /// <summary>
+        /// タイマーリセット
+        /// </summary>
+        private void ResetWatch()
+        {
+            if (stopFlag == true)
+            {
+                StartTime = DateTime.MinValue;
+            }
+            DispTime = "00:00:000";
+        }
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        public StopWatchViewModel()
+        {
+            StartTime = DateTime.MinValue;
+            dispatcherTimer = new DispatcherTimer(DispatcherPriority.Normal);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            stopFlag = true;
+        }
+
+        /// <summary>
+        /// タイマーのインターバルで発生する画面表示メソッド
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            // 起動時間から現在までの経過時間を算出
+            starttime = DateTime.Now.Subtract(StartTime);
+
+            // 表示されている経過時間に新たに経過時間を追加する。
+            //DispTime = sumtime.Add(starttime).ToString(@"mm\:ss\:fff");
+            DispTime = starttime.ToString(@"mm\:ss\:fff");
+        }
+    }
+    public class DelegateCommand : ICommand
+    {
+        /// <summary>
+        /// 現在の状態でこのコマンドを実行できるかどうかを判断するメソッドを定義します。
+        /// </summary>
+        private Func<bool> canExecute;
+
+        /// <summary>
+        /// 現在の状態でこのコマンドを実行できるかどうかを判断するメソッドを定義します。
+        /// </summary>
+        private Action execute;
+
+        /// <summary>
+        /// コマンドの起動時に実行するメソッドを指定してインスタンスを生成、初期化します。
+        /// </summary>
+        /// <param name="execute">コマンドの起動時に実行するメソッド</param>
+        public DelegateCommand(Action execute)
+        {
+            this.execute = execute;
+            this.canExecute = null;
+        }
+
+        /// <summary>
+        /// コマンドの起動時に実行するメソッドとコマンドを実行するかどうかを返すメソッドを指定してインスタンスを生成、初期化します。
+        /// </summary>
+        /// <param name="execute">コマンドの起動時に実行するメソッド</param>
+        /// <param name="canExecute">コマンドを実行するかどうかを返すメソッド</param>
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// コマンドを実行するかどうかに影響するような変更があった場合に発生します。
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// 現在の状態でこのコマンドを実行できるかどうかを返します。
+        /// </summary>
+        /// <param name="parameter">コマンドで使用されたデータ。 コマンドにデータを渡す必要がない場合は、このオブジェクトを null に設定されます。 </param>
+        /// <returns>このコマンドを実行できる場合は true。それ以外の場合は false。</returns>
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute == null)
+            {
+                return true;
+            }
+
+            return this.canExecute();
+        }
+
+        /// <summary>
+        /// コマンドの起動時に呼び出されます。
+        /// </summary>
+        /// <param name="parameter">コマンドで使用されたデータ。 コマンドにデータを渡す必要がない場合は、このオブジェクトを null に設定されます。</param>
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+    }
+}
+
+    
